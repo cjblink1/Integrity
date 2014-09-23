dc55c359101615ad356e38e4244c80c7697b5849
cjblink1 <cjblink1@cjblink120.net>
2014-09-23 14:56:21 -0400

    copied existing code

--- src/com/mk347/CreateWorkspace/CommandMan.java
+++ src/com/mk347/CreateWorkspace/CommandMan.java
@@ -1,5 +1,145 @@
 package com.mk347.CreateWorkspace;
 
-public class CommandMan {
+	import java.io.IOException;
+	import java.util.Iterator;
 
-}
+	import com.mks.api.CmdRunner;
+	import com.mks.api.Command;
+	import com.mks.api.IntegrationPoint;
+	import com.mks.api.IntegrationPointFactory;
+	import com.mks.api.Session;
+	import com.mks.api.response.APIException;
+	import com.mks.api.response.Field;
+	import com.mks.api.response.Response;
+	import com.mks.api.response.WorkItem;
+	import com.mks.api.response.WorkItemIterator;
+
+	/**
+	 * Command man helps to manage commands and connect to the server. 
+	 * It makes everything with the Java API for PTC Integrity easier to work with
+	 * 
+	 * @author MK347, Connor Boyle
+	 * @version 8/21/2014
+	 *
+	 */
+
+	public class CommandMan {
+		
+
+		private String username;
+		private String password;
+		
+		private CmdRunner cr = null;
+		private Session session = null;
+		
+		/**
+		 * Connects to the Integration Point and creates Session and CmdRunner
+		 * @param hostname - hostname for integrity server
+		 * @param port - port of integrity server
+		 * @param username - username used to connect to server
+		 * @param password - password used to connect to server
+		 * @return - true if connected; false if error
+		 */
+		public boolean connect(String hostname, int port)
+		{
+			this.username = "mk347";
+			this.password = "Pbandj14";
+			
+			
+			try {
+				boolean useClientIP = true;
+	                        if (useClientIP) {
+	                            // Client Integration Point
+	                            IntegrationPoint ip = IntegrationPointFactory.getInstance().createLocalIntegrationPoint(4, 12);
+	                            ip.setAutoStartIntegrityClient(true);
+	                            session = ip.getCommonSession(username, password);
+	                            session.setAutoReconnect(true);
+	                            session.setDefaultHostname(hostname);
+	                            session.setDefaultPort(port);
+	                            session.setDefaultUsername(username);
+	                            session.setDefaultPassword(password);
+	                        } else {
+	                            // Server Integration Point
+	                            session = IntegrationPointFactory.getInstance().createIntegrationPoint(hostname, port, 4, 12).createSession(username, password);
+	                            session.setAutoReconnect(true);
+	                        }
+	                        cr = session.createCmdRunner();
+	                                  
+	                        
+	                    } 
+	                catch (APIException apie) {
+				System.out.println("Error occurred during initialization: "
+						+ apie.getMessage());
+				apie.printStackTrace();
+				return false;
+			}
+			
+			return true;
+		}
+		/**
+		 * Executes the command and returns the response object
+		 * @param cmd - command object to be executed
+		 * @return - Response to be parsed
+		 */
+		public Response execute(Command cmd)
+		{
+			
+			try {
+				return cr.execute(cmd);
+			} catch (APIException e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+				return null;
+			}
+		}
+		
+
+		/**
+		 * Prints the Response work items to the console
+		 * @param response - from the executed command
+		 */
+		public void printResponse(Response response)
+		{
+			
+			try{
+				if (response != null) {
+		            System.out.println("Response not null");
+		            WorkItemIterator wii = response.getWorkItems();
+		            while (wii.hasNext()) {
+		            	WorkItem wi = wii.next();
+		            	@SuppressWarnings("unchecked")
+						Iterator<Field> iterator = wi.getFields();
+		            	while (iterator.hasNext()) {
+		            		Field field = iterator.next();
+		            		System.out.println(field.getName() + " : "
+		            				+ field.getValueAsString());
+		            	}
+		            }
+				
+				}
+			}catch(APIException e)
+			{
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}
+		}
+		
+		
+		/**
+		 * Closes the server connection
+		 */
+		public void close()
+		{
+			
+			try {
+				session.release();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (APIException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+
--- src/com/mk347/CreateWorkspace/Main.java
+++ src/com/mk347/CreateWorkspace/Main.java
@@ -1,11 +1,42 @@
 package com.mk347.CreateWorkspace;
 
+
+import com.mks.api.Command;
+import com.mks.api.Option;
+import com.mks.api.OptionList;
+import com.mks.api.response.Response;
+
 public class Main {
 	
-	public static void main (String[] args)
+	public static void main(String[] args)
 	{
 		
+		//Label to filter - can we filter on more than one label?
+		String label = "Communication Manager 05.00.03"; 
 		
+		//Some of the following code may throw an exeption
+		try{
+		//Command manager - helps manage the backend of server connection
+		CommandMan man = new CommandMan();	
+		man.connect("integrity.cummins.com", 7002);
+		//Create Sandbox
+		Command cmd = new Command(Command.SI, "createsandbox");
+		OptionList o = new OptionList();
+		o.add("project", "/core/core2_com_mgr/project.pj");
+		o.add(new Option("recurse"));
+		o.add("scope","anyrevlabellike:"+label);
+		cmd.setOptionList(o);
+		cmd.addSelection("D:\\Users\\mk347\\Desktop\\test5");
+		
+		//Execute creation command
+		Response r = man.execute(cmd);
+		
+		//Print out sandbox ID/directory
+		System.out.println(r.getResult().getPrimaryValue().getId());
+		man.close();
+		
+		}catch(Exception e){}
+	
 	}
 
 }
