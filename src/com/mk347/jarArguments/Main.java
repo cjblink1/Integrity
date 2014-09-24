ced1676f44da7911e5c37fe34fdf32eb8fe753a4
cjblink1 <cjblink1@cjblink120.net>
2014-09-24 16:18:50 -0400

    created jArgs tester

---
+++ src/com/mk347/jarArguments/Main.java
@@ -0,0 +1,17 @@
+package com.mk347.jarArguments;
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		
+		for (String s: args)
+		{
+			
+			System.out.println(s);
+		}
+		
+		System.out.println("DONE");
+	}
+
+}
